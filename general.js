a047e896d77ac526578c7bb68466fafcb818d9bb
Ernestina77 <69101468+Ernestina77@users.noreply.github.com>
2020-10-02 12:00:48 -0600

    final parcial

---
+++ general.js
@@ -0,0 +1,56 @@
+//variables
+const atributo = document.querySelector('.elemento').getAttribute
+('descripcion');
+console.log(atributo);
+
+const paises=document.querySelectorAll('.elemento');
+console.log(paises);
+
+const ElSalvador=paises[1]
+console.log(paises);
+
+const Honduras=paises[2]
+console.log(Honduras);
+
+const Nicaragua=paises[3]
+console.log(Nicaragua);
+
+const CostaRica=paises[4]
+console.log(CostaRica);
+
+const Panama=paises[5]
+console.log(Panama);
+
+
+
+
+const lugar=document.querySelectorAll(',elemento');
+const tarjeta=document.querySelector('#tarjeta');
+const texto=document.querySelector('#texto');
+
+lugares.forEach(function cadaPais(lugar){
+    lugar.addEventListener('click', cambiarlugar);
+
+ })
+
+ 
+
+    
+//Eventos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
